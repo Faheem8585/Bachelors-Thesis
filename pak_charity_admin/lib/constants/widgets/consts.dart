59ff5bcfc4e7f06e5d5842e97b191873c810ad89
Faheem <faheem@Naeems-MacBook-Pro.local>
2025-10-18 19:28:15 +0200

    Remove Secret Stripe

--- pak_charity_admin/lib/constants/widgets/consts.dart
+++ pak_charity_admin/lib/constants/widgets/consts.dart
@@ -9,6 +9,4 @@ const OutlineInputBorder outlineInputBorder = OutlineInputBorder(
 );
 
 const String stripeKey =
-    'pk_test_51L2ySrFA9SdVfjX6vdz0gEReE6hOUFP98XLtjPwAwAfKbR9F3241hdNNUrAcXXLNKYWmtD6xQrlIXfb2rsOPCv5u00u784rvKl';
-const String stripeSecretKey =
-    'sk_test_51L2ySrFA9SdVfjX6OPuvTpRuQLYcafcR84klNn5lZ5vJBrokcPjI1BYlY6BGKj75LbP7mQynLrr52zLUZhOFqYjK006OyrjVkR';
+    'pk_test_51SJdWPJvyWKfkzrPhB3ZoMQ0sAJTPaBj7QqcmX94AyVCkJbnlfPeNwsnQf2f0UkzlZTm5ox6IkzJ3zZrF9d3YmVP00mk3SmMc5';
